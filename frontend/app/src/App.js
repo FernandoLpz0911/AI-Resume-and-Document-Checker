33ab451006023d8b4ec7696862ce38b85b90484f
Andres Popoca <apopo4@uic.edu>
2025-11-11 17:38:31 -0600

    josh update v2

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import josh from './josh.png'
 import './App.css';
 import './balls.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={josh} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
